a0fc9308473ae4ab456b8362a317450e6eb3dd86
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-08-01 18:59:01 +0100

    Moved code from main.gd to global.gd

--- scripts/global.gd
+++ scripts/global.gd
@@ -12,3 +12,12 @@ var textbox_visible = false
 var textbox_on_cooldown = false
 
 #@onready var music: AudioStreamPlayer = get_tree().root.Music
+
+func _process(_delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if Window.MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
