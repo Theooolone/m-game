b35c1a1d266609803e416e31b7f646e667053f12
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-08-01 18:41:06 +0100

    Added window toggle with f11

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,3 +10,8 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if Window.MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
