a091a4757d3424e591d6aec509f70870f35296b4
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-08-02 02:56:09 +0100

    Border's state gets saved

--- scripts/Border.gd
+++ scripts/Border.gd
@@ -3,7 +3,10 @@ extends StaticBody2D
 var interactedids = []
 
 func _ready():
+	if Global.config.get_value("misc", "barrier_passed"):
+		queue_free()
 	Global.id_interaction.connect(_on_id_interaction)
+	
 
 func contains(id):
 	return interactedids.find(id) != -1
@@ -12,4 +15,6 @@ func _on_id_interaction(id):
 	if not contains(id):
 		interactedids.append(id)
 	if contains(1) and contains(2) and contains(3):
+		Global.config.set_value("misc", "barrier_passed", true)
+		Global.config.save(Global.configpath)
 		queue_free()
