0f25922fa7ab1b893ac43b53729600f297bbe0e6
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-08-05 21:37:12 +0100

    Fixed glitch involving F11 key

--- scripts/global.gd
+++ scripts/global.gd
@@ -34,9 +34,10 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
+	
 	if Input.is_action_just_pressed("toggle_fullscreen"):
-		if Window.MODE_FULLSCREEN:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+		if get_window().mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
