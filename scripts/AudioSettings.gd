0cafca4424d6da2a3af713d67a4ef07029c8063d
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-08-12 00:19:56 +0100

    Fixed error if audio sliders at default

--- scripts/AudioSettings.gd
+++ scripts/AudioSettings.gd
@@ -3,12 +3,14 @@ extends CanvasLayer
 
 
 func _ready():
-	for key in Global.config.get_section_keys("audio"):
-		var value = Global.config.get_value("audio", key)
-		var slider_node = $Panel/MarginContainer/HBoxContainer.get_node(key)
-		slider_node.get_node("VSlider").value = value
-		if value == 69:
-			slider_node.get_node("Value").text = "69% :)"
+	
+	if Global.config.has_section("audio"):
+		for key in Global.config.get_section_keys("audio"):
+			var value = Global.config.get_value("audio", key)
+			var slider_node = $Panel/MarginContainer/HBoxContainer.get_node(key)
+			slider_node.get_node("VSlider").value = value
+			if value == 69:
+				slider_node.get_node("Value").text = "69% :)"
 			
 	get_node("/root/Main/Music").play()
 
