381b3ca5ef1317d51765f0ebb7510efed0bef9ea
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-08-20 17:36:28 +0100

    Harder difficulties now have to be unlocked

--- scripts/difficulty.gd
+++ scripts/difficulty.gd
@@ -6,10 +6,21 @@ func _ready():
 	%EasyButton/Label.text = "Easy (" + str(10*easy_highscore) + ")"
 	var normal_highscore = Global.get_config_value("cat_sim_highscores", "difficulty_1", 0)
 	%NormalButton/Label.text = "Normal (" + str(10*normal_highscore) + ")"
+	
 	var hard_highscore = Global.get_config_value("cat_sim_highscores", "difficulty_2", 0)
-	%HardButton/Label.text = "Hard (" + str(10*hard_highscore) + ")"
+	if normal_highscore >= 250:
+		%HardButton/Label.text = "Hard (" + str(10*hard_highscore) + ")"
+	else:
+		%HardButton.disabled = true
+		%HardButton/Label.text = "Difficulty Locked"
+	
 	var pain_highscore = Global.get_config_value("cat_sim_highscores", "difficulty_5", 0)
-	%PainButton/Label.text = "Pain (" + str(10*pain_highscore) + ")"
+	
+	if hard_highscore >= 250:
+		%PainButton/Label.text = "Pain (" + str(10*pain_highscore) + ")"
+	else:
+		%PainButton.disabled = true
+		%PainButton/Label.text = "Difficulty Locked"
 
 
 func _on_easy_button_pressed():
