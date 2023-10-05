5b990856c5b38858c9331c7445a4ca6e510b8539
Theooolone <110533961+Theooolone@users.noreply.github.com>
2023-10-05 17:49:45 +0100

    Added scrolling background

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,3 +7,10 @@ func _on_play_button_pressed():
 
 func _on_settings_button_pressed():
 	pass # Replace with function body.
+
+@onready var background = $TileMap
+
+func _process(delta):
+	background.position = background.position.move_toward(Vector2(-1088,1104), 5*delta)
+	if background.position == Vector2(-1088,1104):
+		background.position = Vector2.ZERO
